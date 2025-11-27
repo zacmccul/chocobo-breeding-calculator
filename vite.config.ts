26a9225bbb0f6157ff9a38e3297c538c74179474
Zachary McCullough <zam.mccullough@gmail.com>
2025-11-26 16:15:15 -0800

    feat: add GitHub Actions workflow for deployment and update homepage URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/chocobo-breeding-calculator/',
   plugins: [
     react(),
     viteSingleFile()
